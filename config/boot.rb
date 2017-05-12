38023991a073fb3f6f80e5181b16706fca1cdc77
Thomas Prost <thomasprost@warpjapan.com>
2017-05-12 16:06:43 +0900

    Initial commit

---
+++ config/boot.rb
@@ -0,0 +1,13 @@
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+
+require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+require 'rails/commands/server'
+
+module DefaultOptions
+  def default_options
+    super.merge!(Host: '0.0.0.0')
+  end
+end
+
+Rails::Server.send(:prepend, DefaultOptions)
